c8cfe51cc5b982164285fdfb00d51610e35b1df7
Wondaeng <71166964+Wondaeng@users.noreply.github.com>
2022-07-17 17:45:11 +0900

    Update main.py

--- tiling/main.py
+++ tiling/main.py
@@ -1,5 +1,6 @@
 import utils
 import parsing
+import os
 
 if __name__ == '__main__':
     # Set basic directories
@@ -15,20 +16,29 @@ if __name__ == '__main__':
     row = 4
     col = 6
 
+
     dst_tile = './images_v3_tiled'
     out_name = 'dataset_tiled.json'
-    js_tiled_pth = os.join.path(dst_tile, out_name)
+    js_tiled_pth = os.path.join(dst_tile, out_name)
 
+    print(f"Start tiling the images into {row}*{col} pieces...")
     utils.tiling_img(row, col, src, dst=dst_tile, names=img_names)
+    print("Tiling images finished!")
+
+    print("Start tiling corresponding bboxes...")
     utils.tiling_bbox(row, col, pth_json=js_save_pth, dst=dst_tile, out_name=out_name)
+    print("Tiling bboxes finished!")
+
 
     pos_img_names = parsing.get_pos_neg_names(js_tiled_pth, pos=True)
     neg_img_names = parsing.get_pos_neg_names(js_tiled_pth, pos=False)
 
-    image_folder_pth = './iamges_v3_classification'
+    image_folder_pth = './images_v3_classification'
 
-    dst_tile_pos = os.path.join(dst_tile, 'positive')
-    dst_tile_neg = os.path.join(dst_tile, 'negative')
+    dst_tile_pos = os.path.join(image_folder_pth, 'positive')
+    dst_tile_neg = os.path.join(image_folder_pth, 'negative')
 
+    print(f"Start parsing and copying positive/negative images into {image_folder_pth} folder...")
     utils.cp_imgs_with_names(pos_img_names, dst_tile, dst_tile_pos)
     utils.cp_imgs_with_names(neg_img_names, dst_tile, dst_tile_neg)
+    print("Parsing images finished!")
